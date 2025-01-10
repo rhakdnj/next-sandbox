3f02654977119645be39237d3f26d5e76e7ca3e0
rhakdnj <chungjm0711@gmail.com>
2025-01-10 14:51:34 +0900

    feat: query string 가져올 때

--- apps/onebite-next/section02/src/pages/search/index.tsx
+++ apps/onebite-next/section02/src/pages/search/index.tsx
@@ -1,5 +1,10 @@
 import {useRouter} from "next/router";
 
+/**
+ * @description
+ * request parameter 존재 할 때 해당 값을 읽기 위해 해당 컴포넌트를 한번 더 읽는다.
+ * 이 때, router.query = {q: "requestParameter" }가 매꿔진다.
+ */
 export default function Page() {
   const router = useRouter();
 
